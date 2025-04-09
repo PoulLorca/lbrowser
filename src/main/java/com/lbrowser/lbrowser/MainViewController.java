53f4a6379de9ab2c61ba0a4c7a56e4d7d29d7dfb
PoulLorca <lorcapoul@gmail.com>
2025-04-09 15:27:04 -0400

    Implement main application structure with UI components and navigation functionality

--- src/main/java/com/lbrowser/lbrowser/MainViewController.java
+++ src/main/java/com/lbrowser/lbrowser/MainViewController.java
@@ -1,4 +1,69 @@
 package com.lbrowser.lbrowser;
 
-public class MainViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainViewController implements Initializable {
+    @FXML
+    public WebView web_view;
+    @FXML
+    public TextField web_url;
+    @FXML
+    public Button reload_button;
+    @FXML
+    public Button prev_button;
+    @FXML
+    public Button next_button;
+
+    private WebEngine webEngine;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        webEngine = web_view.getEngine();
+        String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/605.1 (KHTML, like Gecko) Lbrowser/1.0";
+        webEngine.setUserAgent(userAgent);
+
+        webEngine.locationProperty().addListener((observable, oldValue, newValue) -> {
+           web_url.setText(newValue);
+        });
+
+        web_url.setOnAction(event -> {
+            String url = web_url.getText();
+            if(!url.startsWith("http://") && !url.startsWith("https://")) {
+                url = "https://" + url;
+            }
+            webEngine.load(url);
+        });
+
+        // Load a default page
+        webEngine.load("https://www.google.com");
+    }
+
+    public void goBack(){
+        if(webEngine.getHistory().getCurrentIndex() > 0){
+            webEngine.getHistory().go(-1);
+        }
+    }
+    public void goForward(){
+        if(webEngine.getHistory().getCurrentIndex() < webEngine.getHistory().getEntries().size() - 1){
+            webEngine.getHistory().go(1);
+        }
+    }
+    public void reload(){
+        webEngine.reload();
+    }
+    public void zoomIn(){web_view.setZoom(web_view.getZoom() + 0.1);}
+    public void zoomOut(){web_view.setZoom(web_view.getZoom() - 0.1);}
+    public void zoomReset(){web_view.setZoom(1.0);}
+    public void showHistory(){
+        System.out.println(webEngine.getHistory());
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.lbrowser.lbrowser {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires javafx.web;
 
     opens com.lbrowser.lbrowser to javafx.fxml;
     exports com.lbrowser.lbrowser;
