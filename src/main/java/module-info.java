6e9b461ec05ea27a985cb2bddb38060bbe2a5370
PoulLorca <lorcapoul@gmail.com>
2025-04-25 15:07:01 -0400

    Refactor module dependencies and remove unused JavaFX components in module-info and pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,8 @@
 module com.lbrowser.lbrowser {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires javafx.web;
     requires qtjambi.webenginewidgets;
     requires qtjambi.webenginecore;
     requires qtjambi.network;
 
-    opens com.lbrowser.lbrowser to javafx.fxml;
     exports com.lbrowser.lbrowser;
     exports com.lbrowser.lbrowser.media;
-    opens com.lbrowser.lbrowser.media to javafx.fxml;
 }
